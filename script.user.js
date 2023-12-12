a52c5d942fdfb040d52b303a121e94590d90e28d
Phong Pham <70873548+iamSlightlyWind@users.noreply.github.com>
2023-12-12 12:35:13 +0700

    Download all classes timetable.

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Class Information Selector
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.62
 // @description  Download the class timetable
 // @author       iam.slightlywind@themajorones.dev
 // @match        https://fap.fpt.edu.vn/*
@@ -10,71 +10,57 @@
 (function () {
     'use strict';
 
-    // Function to get the index of the currently selected option
     function getSelectedIndex() {
         var classSelect = document.getElementById('ctl00_mainContent_dllCourse');
         return classSelect.selectedIndex;
     }
 
-    // Function to set the selected index of the class
     function setSelectedIndex(index) {
         var classSelect = document.getElementById('ctl00_mainContent_dllCourse');
         classSelect.selectedIndex = index;
         classSelect.dispatchEvent(new Event('change'));
     }
 
-    // Function to download text content
     function downloadTextContent(className) {
-        // Get the text content of the element
         var textContent = document.getElementById('ctl00_mainContent_lblNewSlot').innerText.trim();
-
-        // Create a Blob from the text content
         const blob = new Blob([textContent], { type: 'text/plain' });
-
-        // Create a link element
         const link = document.createElement('a');
-
-        // Set the link's attributes
         link.href = URL.createObjectURL(blob);
         link.download = className + '.txt';
-
-        // Append the link to the document
         document.body.appendChild(link);
-
-        // Trigger a click on the link to start the download
         link.click();
-
-        // Remove the link from the document
         document.body.removeChild(link);
     }
 
-    // Create the button
     var customButton = document.createElement('button');
-    customButton.innerHTML = 'Select Next Class';
+    customButton.innerHTML = 'Download All Classes';
     customButton.style.position = 'fixed';
     customButton.style.top = '10px';
     customButton.style.left = '10px';
     customButton.addEventListener('click', function () {
-        // Get the current index
-        var classSelect = document.getElementById('ctl00_mainContent_dllCourse');
-        var currentIndex = getSelectedIndex();
-
-        // Get the class name
-        var className = classSelect.options[currentIndex].text;
-
-        // Download the text content
-        downloadTextContent(className);
-
-        // Calculate the next index
-        var nextIndex = (currentIndex + 1) % classSelect.options.length;
-
-        // Check if it's the last class
-        if (nextIndex !== 0) {
-            // Select the next class
-            setSelectedIndex(nextIndex);
-        }
+        localStorage.setItem('scriptRunning', 'true');
+        setSelectedIndex(0);
     });
 
-    // Append the button to the body
     document.body.appendChild(customButton);
+
+    var scriptRunning = localStorage.getItem('scriptRunning');
+    if (scriptRunning === 'true') {
+        if (window.location.href === 'https://fap.fpt.edu.vn/Default.aspx') {
+            localStorage.removeItem('scriptRunning');
+            return;
+        }
+
+        var classSelect = document.getElementById('ctl00_mainContent_dllCourse');
+        var currentIndex = getSelectedIndex();
+        var className = classSelect.options[currentIndex].text;
+        downloadTextContent(className);
+        var nextIndex = (currentIndex + 1) % classSelect.options.length;
+
+        if (nextIndex !== 0) {
+            setSelectedIndex(nextIndex);
+        } else {
+            localStorage.removeItem('scriptRunning');
+        }
+    }
 })();
